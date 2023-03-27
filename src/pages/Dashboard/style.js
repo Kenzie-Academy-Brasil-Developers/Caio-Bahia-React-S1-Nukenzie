227fc046d89f4ab6f4a0b13e51fb9037926c3a60
Caio <caiob1305@gmail.com>
2023-03-27 10:46:59 -0300

    Final Commit

--- src/pages/Dashboard/style.js
+++ src/pages/Dashboard/style.js
@@ -4,10 +4,24 @@ export const MainStyled = styled.main`
   display: flex;
   align-items: center;
   flex-direction: column;
+  @media (min-width: 770px) {
+    flex-direction: row;
+    padding: 0px 20px;
+    padding-bottom: 20px;
+    height: 90vh;
+  }
 `
 export const SectionStyledForm = styled.section``
 
 export const SectionStyledList = styled.section`
   width: 100%;
   margin: 20px;
+  @media (min-width: 770px) {
+    display: flex;
+    flex-direction: column;
+    -webkit-box-pack: start;
+    justify-content: flex-start;
+    align-items: flex-start;
+    height: 100%;
+  }
 `
